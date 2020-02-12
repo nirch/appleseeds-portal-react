e8f144190b285e319af3c550002133382d59d613
Maor Berkover <mahosh1983@gmail.com>
2020-02-12 14:22:29 +0200

    Checking the compenent and fixings

--- src/components/TableComponent/PortalTable.js
+++ src/components/TableComponent/PortalTable.js
@@ -1,29 +1,29 @@
 import React, { Component } from 'react';
 
 
-export default class Table extends Component {
+export default class PortalTable extends Component {
     constructor(props) {
         super(props);
     }
     render(){
-        const { headers, data, handleClick } = this.props;
+        const { headers, data ,handleClick} = this.props;
         let rows = [];
-        for(i=0 ; i<data.length ; i++){
+        for(let i=0 ; i<data.length ; i++){
             let currentRow = [];
-            for(j=0 ; j<headers.length ; j++){
+            for(let j=0 ; j<headers.length ; j++){
                 let currentData;
-                if(data[i][headers][j].key) {
-                    currentData = <td onClick={handleClick(data)}>{data[i][headers][j].key}</td>
+                if(data[i][headers[j].key]) {
+                    currentData = <td onClick={handleClick(data)}>{data[i][headers[j].key]}</td>;
                 } else {
                     currentData = <td></td>;
                 }
-                currentRow.push(data[i][headers][j].key);
+                currentRow.push(currentData);
             }
             rows.push(<tr>{currentRow}</tr>);
         }
         let headerRow = [];
-        for(i=0 ; i<headers.length ; i++){
-            headerRow.push(<td>{headers[i].key}</td>);
+        for(let i=0 ; i<headers.length ; i++){
+            headerRow.push(<th>{headers[i].key}</th>);
         }
         return(
             <table>
