8d878432aaf58c587ae36d08b9162601094ae10b
Maor Berkover <mahosh1983@gmail.com>
2020-02-12 13:58:12 +0200

    Creating the table

--- src/components/TableComponent/table.js
+++ src/components/TableComponent/table.js
@@ -7,11 +7,34 @@ export default class Table extends Component {
     }
     render(){
         const { headers, data, handleClick } = this.props;
-        const numOfColumns = headers.length;
-        const numOfRows = data.length;
+        let rows = [];
+        for(i=0 ; i<data.length ; i++){
+            let currentRow = [];
+            for(j=0 ; j<headers.length ; j++){
+                let currentData;
+                if(data[i][headers][j].key) {
+                    currentData = <td onClick={handleClick(data)}>{data[i][headers][j].key}</td>
+                } else {
+                    currentData = <td></td>;
+                }
+                currentRow.push(data[i][headers][j].key);
+            }
+            rows.push(<tr>{currentRow}</tr>);
+        }
+        let headerRow = [];
+        for(i=0 ; i<headers.length ; i++){
+            headerRow.push(<td>{headers[i].key}</td>);
+        }
         return(
             <table>
-
+                <thead>
+                    <tr className="headerRow">
+                        {headerRow}
+                    </tr>
+                </thead>
+                <tbody>
+                   {rows}
+                </tbody>
             </table>
         )
     }
