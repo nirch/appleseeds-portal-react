db41bec42d63c9bb5b451b872bf879239438d491
Eyal Lev <eyal18yaffa03@gmail.com>
2020-02-13 00:05:14 +0200

    page in the correct sizes

--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -5,6 +5,7 @@ import server from '../../shared/server'
 import { Redirect } from 'react-router-dom'
 import ActiveUserContext from '../../shared/activeUserContext'
 
+
 const LoginPage = (props) => {
     const { handleLogin } = props;
     const [email, setEmail] = useState("");
@@ -13,13 +14,12 @@ const LoginPage = (props) => {
 
     const login = () => {
 
-        if(!email || !pwd)
-		{
-			alert("נא להזין פרטי משתמש");
-			return;
+        if (!email || !pwd) {
+            alert("נא להזין פרטי משתמש");
+            return;
         }
-        
-        const data = {email, pass: pwd};
+
+        const data = { email, pass: pwd };
         server(null, data, "login").then(res => {
             console.log(res);
             if (res.data.error) {
@@ -38,24 +38,44 @@ const LoginPage = (props) => {
 
     return (
 
+
+
         <Container className="p-login">
-            <h1>התחברות</h1>
-            <Form>
+            <img src="drawable-hdpi/01.png" />
+
+            <div className="loginbutton">
+
+                <input value={email} type="email" placeholder="אימייל" onChange={e => setEmail(e.target.value)} >
+                </input>
+            </div>
+            <div className="loginbutton">
+
+                <input value={email} placeholder="סיסמא" type="password" placeholder="סיסמא" onChange={e => setEmail(e.target.value)} >
+                </input>
+            </div>
+            <div className="loginbutton">
+                <button type="button" onClick={login}>התחברות</button>
+
+
+            </div>
+
+            {/* <Form>
                 <Form.Group controlId="formBasicEmail">
                     <Form.Label></Form.Label>
-                    <Form.Control value={email} type="email" placeholder="אימייל" onChange={e => setEmail(e.target.value)}/>
+                    <Form.Control value={email} type="email" placeholder="אימייל" onChange={e => setEmail(e.target.value)} />
                 </Form.Group>
 
                 <Form.Group controlId="formBasicPassword">
                     <Form.Label></Form.Label>
-                    <Form.Control value={pwd} type="password" placeholder="סיסמה" onChange={e => setPwd(e.target.value)}/>
+                    <Form.Control value={pwd} type="password" placeholder="סיסמה" onChange={e => setPwd(e.target.value)} />
                 </Form.Group>
-
-                <Button variant="primary" type="button" onClick={login}>
+ <input value={email} placeholder="כניסה" type="email" placeholder="אימייל" onChange={e => setEmail(e.target.value)} >
+  </input>        
+ <Button variant="primary" type="button" onClick={login}>
                     התחבר
                 </Button>
-            </Form>
-        </Container>
+           </Form> */}
+        </Container >
     );
 }
 
