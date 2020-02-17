97474f149f2282c106ba2230ac1381da60fb3aea
comradem <pperedot@gmail.com>
2020-02-17 14:40:20 +0200

    import fixes

--- src/pages/users/UsersPage.js
+++ src/pages/users/UsersPage.js
@@ -5,14 +5,16 @@ import ActiveUserContext from '../../shared/activeUserContext'
 import {Redirect} from 'react-router'
 import PortalTable from "../../components/TableComponent/PortalTable";
 import PortalSearchPager from '../../components/search/PortalSearchPager'
-import {PortalButtonSet} from '../../components/navbar/PortalButtonSet'
+import PortalButtonSet from '../../components/navbar/PortalButtonSet'
 import server from '../../shared/server'
 
 const UsersPage = (props) => {
-    const {handleLogout} = props;
+    const {handleLogout, } = props;
     const activeUser = useContext(ActiveUserContext);
 
     const [data, setData] = useState([]);
+    const [searchStr, setSearchStr] = useState('');
+
 
     const [headers, setHeaders] = useState([{key: "userid", header: "מס' מזהה"}, {
         key: "firstname",
@@ -60,12 +62,12 @@ const UsersPage = (props) => {
         }
     };
 
-    const handleUserType = () => {
-        console.log('i am hunduru ro event');
+    const handleUserType = (item) => {
+        console.log('i am hunduru ro event ' + item.label);
     };
 
     const callPageData = (index) => {
-        console.log('page index '+index);
+        console.log('page index ' + index);
     };
 
     const numOfPages = (data.length % 10 + 1);
@@ -73,11 +75,12 @@ const UsersPage = (props) => {
         <div>
             <PortalNavbar handleLogout={handleLogout}/>
             <h1>משתמשים</h1>
-            <PortalSearchPager placeholder='חיפוש משתמש' handleSearch={(e) => handleSearch(e)} pages={numOfPages} pageChange={(index)=> callPageData(index)}>search
+            <PortalSearchPager placeholder='חיפוש משתמש' handleSearch={(e) => handleSearch(e)} pages={numOfPages}
+                               pageChange={(index) => callPageData(index)}>search
                 bar</PortalSearchPager>
             <PortalTable headers={headers} data={data} handleClick={(e) => console.log(e.target)}>blah</PortalTable>
             <PortalButtonSet buttons={[{key: 0, label: 'פעילים'}, {key: 1, label: 'לא פעילים'}]}
-                             handleClick={(item) => handleUserType(item)}>blea</PortalButtonSet>
+                             handleClick={(item) => handleUserType(item)} activeKey={'0'}>blea</PortalButtonSet>
         </div>
     );
 };
