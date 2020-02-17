35ed8591a6319d43ac0ef700dfb12381f0a13ed9
comradem <pperedot@gmail.com>
2020-02-17 14:13:22 +0200

    minor fix -> callback for pagination

--- src/pages/users/UsersPage.js
+++ src/pages/users/UsersPage.js
@@ -5,25 +5,25 @@ import ActiveUserContext from '../../shared/activeUserContext'
 import {Redirect} from 'react-router'
 import PortalTable from "../../components/TableComponent/PortalTable";
 import PortalSearchPager from '../../components/search/PortalSearchPager'
+import {PortalButtonSet} from '../../components/navbar/PortalButtonSet'
 import server from '../../shared/server'
 
 const UsersPage = (props) => {
     const {handleLogout} = props;
     const activeUser = useContext(ActiveUserContext);
 
-
     const [data, setData] = useState([]);
 
-
-    const [headers, setHeaders] = useState([{key: "firstname", header: "שם פרטי"}, {
+    const [headers, setHeaders] = useState([{key: "userid", header: "מס' מזהה"}, {
+        key: "firstname",
+        header: "שם פרטי"
+    }, {
         key: "lastname",
         header: "שם משפחה"
     }, {key: 'email', header: 'דוא"ל'}]);
 
-
     //inner functions
     //fetch data from db
-    //TODO
     let payload = {"search": "", "sorting": "userid", "desc": false, "userstatus": 1, "page": -1};
 
     useEffect(() => {
@@ -33,7 +33,6 @@ const UsersPage = (props) => {
                 if (res.data.error) {
                     alert("error in login");
                 } else {
-                    console.log(res.data);
                     setData(res.data);
                 }
             }, err => {
@@ -50,8 +49,9 @@ const UsersPage = (props) => {
     const handleSearch = (searchText) => {
         if (searchText) {
             let newData = data.filter(item => {
-                return (item.fname.toLowerCase().includes(searchText) ||
-                    item.lname.toLowerCase().includes(searchText) ||
+
+                return (item.firstname.toLowerCase().includes(searchText) ||
+                    item.lastname.toLowerCase().includes(searchText) ||
                     item.email.toLowerCase().includes(searchText));
             });
             setData(newData);
@@ -60,15 +60,24 @@ const UsersPage = (props) => {
         }
     };
 
-    debugger
+    const handleUserType = () => {
+        console.log('i am hunduru ro event');
+    };
 
+    const callPageData = (index) => {
+        console.log('page index '+index);
+    };
+
+    const numOfPages = (data.length % 10 + 1);
     return (
         <div>
             <PortalNavbar handleLogout={handleLogout}/>
             <h1>משתמשים</h1>
-            <PortalSearchPager placeholder='חיפוש משתמש' handleSearch={(e) => handleSearch(e)}>search
+            <PortalSearchPager placeholder='חיפוש משתמש' handleSearch={(e) => handleSearch(e)} pages={numOfPages} pageChange={(index)=> callPageData(index)}>search
                 bar</PortalSearchPager>
             <PortalTable headers={headers} data={data} handleClick={(e) => console.log(e.target)}>blah</PortalTable>
+            <PortalButtonSet buttons={[{key: 0, label: 'פעילים'}, {key: 1, label: 'לא פעילים'}]}
+                             handleClick={(item) => handleUserType(item)}>blea</PortalButtonSet>
         </div>
     );
 };
