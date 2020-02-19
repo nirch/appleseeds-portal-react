539f39fb52bc5f59b1e4002fb5d87f9a897cae9d
sigal <ssamburski@gmail.com>
2020-02-19 10:32:40 +0200

    approved hours-infrastracture and worker reported hours component

---
+++ src/pages/hours/ReportedHoursDetailsItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ButtonGroup } from 'react-bootstrap'
+import { useState } from 'react';
+
+const reportedHoursDetailsItem = (props) => {
+    
+    return (
+        <div className="c-reported-hours-details-item">
+      
+        </div>
+    );
+}
+
+export default reportedHoursDetailsItem;
