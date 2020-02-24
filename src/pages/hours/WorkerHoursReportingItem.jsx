a56f4055d67c2803cdde7b4a883d8cacc46c1321
sigal <ssamburski@gmail.com>
2020-02-24 15:37:05 +0200

    approve hours page, accordeon detailed data

--- src/pages/hours/WorkerHoursReportingItem.jsx
+++ src/pages/hours/WorkerHoursReportingItem.jsx
@@ -4,19 +4,17 @@ import './WorkerHoursReportingItem.css'
 
 const WorkerHoursReportingItem = (props) => {
 
-    const [pendingHours,setPendingHours]=useState(0);
-    const [approvedHours,setApprovedHours]=useState(0);
-    const [rejectedHours,setRejectedHours]=useState(0);
-    const [totalHours,setTotalHours]=useState(0);
+    const {workerDetails} =  props;
+
 
     return (
         <div className="c-worker-hours-reporting-item">
-            <div className="worker-name" >שם עובד</div>
+            <div className="worker-name" >{workerDetails.name}</div>
             <div className="worker-params">
-                <div className="d-inline-block hours-total">150</div>
-                <div className="d-inline-block hours-rejected">50</div>
-                <div className="d-inline-block hours-approved">50</div>
-                <div  className="d-inline-block hours-pending">50</div>              
+                <div  className="d-inline-block hours-pending">{workerDetails.pendingHours}</div>
+                <div className="d-inline-block hours-approved">{workerDetails.approvedHours}</div>              
+                <div className="d-inline-block hours-rejected">{workerDetails.rejectedHours}</div>
+                <div className="d-inline-block hours-total">{workerDetails.totalHours}</div>
                 <div className="d-inline-block">
                     <img className="open" src="images/arrow-down.png" alt="open"></img>
                 </div>
