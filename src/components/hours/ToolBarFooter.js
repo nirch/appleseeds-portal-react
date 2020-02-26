9c73d1c8668db82268caa8202f587b39b1d5d219
hilel-dr <57894838+hilel-dr@users.noreply.github.com>
2020-02-26 11:38:35 +0200

    add ToolBarFooter component

---
+++ src/components/hours/ToolBarFooter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ToolBarFooter.css'
+
+const ToolBarFooter = (props) => {
+    const {report} = props;
+
+
+    return (
+        <div class="c-toolbar-footer">
+        <p>Footer</p>
+        </div>    
+    );
+}
+
+export default ToolBarFooter;
--- src/pages/hours/HoursReportPage.js
+++ src/pages/hours/HoursReportPage.js
@@ -5,6 +5,8 @@ import ActiveUserContext from '../../shared/activeUserContext'
 import HoursReportTable from '../../components/hours/HoursReportTable'
 import PortalDayMonthPickerComponent from '../../components/PortalDayMonthPickerComponent/PortalDayMonthPickerComponent'
 import server from "../../shared/server";
+import ToolBarFooter from "../../components/hours/ToolBarFooter";
+
 
 const HoursReportPage = (props) => {
     const { handleLogout } = props;
@@ -108,6 +110,7 @@ const HoursReportPage = (props) => {
             <PortalDayMonthPickerComponent onShowDate={onShowDate}/>
             <div className="p-total-hours">{totalHoursHeader}</div>
             <HoursReportTable reports={reports}/>
+            <ToolBarFooter/>
         </div>
     );
 }
